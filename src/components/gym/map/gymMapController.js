ff51dc0a9b407596255c6ca09c9dbf233a4e4cf2
doman412 <doman412@gmail.com>
2015-05-20 17:06:13 -0400

    testing some canvas hovers

--- src/components/gym/map/gymMapController.js
+++ src/components/gym/map/gymMapController.js
@@ -17,6 +17,9 @@ var GymMapController = BaseController.extend({
     defineScope:function(){
         $(document).ready(function(e) {
             $('img[usemap]').rwdImageMaps();
+            
+            
+            
             console.log("done");
         });
 
@@ -24,7 +27,7 @@ var GymMapController = BaseController.extend({
     },
 
     destroy:function(){
-
+        $('canvas').off('.omgmaps');
     },
 
 
@@ -33,19 +36,30 @@ var GymMapController = BaseController.extend({
     },
 
     highlightMap: function(){
-
-        var map = $("#gymMap")[0];
-        for(var i = 0; i < map.children.length; i++){
-
-            var area = map.children[i];
+        
+        var body = $('body'),
+            map = $("#gymMap"),
+            mapEl = $("#gymMap").get(0),
+            mapAreas = map.find('area');
+        
+        
+        $.each(mapAreas, function(index, object){
+            var object = $(object);
+            var area = object.get(0);
             var coords = area.coords.split(", ");
-            console.log(coords);
 
-            var canvas = document.createElement("canvas");
-
-            //canvas.style.cssText = 'position:absolute';
-            document.body.appendChild(canvas);
-            var ctx = canvas.getContext('2d');
+            //            console.log('coords');
+            //            console.log(coords);
+            
+            // create a canvas
+            var canvas = $('<canvas>').css('display', 'none');
+            // append said canvas
+            body.append(canvas);
+            
+            // grab the canvas context
+            var ctx = canvas.get(0).getContext('2d');
+//            console.log('ctx');
+//            console.log(ctx);
 
             ctx.fillStyle = '#f00';
             ctx.beginPath();
@@ -53,17 +67,17 @@ var GymMapController = BaseController.extend({
             var x = coords[0];
             var y = coords[1];
 
-            console.log("x: " + x);
-            console.log("y: " + y);
+            //            console.log("x: " + x);
+            //            console.log("y: " + y);
             ctx.moveTo(coords[0], coords[1]);
-            console.log(coords.length);
+            //            console.log(coords.length);
             for(var j = 2; j < coords.length-1 ; j += 2 ){
                 x = coords[j];
                 y = coords[j + 1];
 
-                console.log("x: " + x);
-                console.log("y: " + y);
-                console.log("j: " + j);
+                //                console.log("x: " + x);
+                //                console.log("y: " + y);
+                //                console.log("j: " + j);
 
                 ctx.lineTo(x, y);
             }
@@ -77,12 +91,26 @@ var GymMapController = BaseController.extend({
             ctx.closePath();
             ctx.fill();
 
-//            ctx.closePath();
-//            ctx.fill();
+            //            ctx.closePath();
+            //            ctx.fill();
 
-//            console.log(canvas);
+            //            console.log(canvas);
+            
+            // create an area mouseenter event listener
+            object.on('mouseenter.omgmaps', (function(canvas){
+                return function(event){
+                    canvas.css('display', 'inline-block');
+                }
+            })(canvas));
 
-        }
+            object.on('mouseleave.omgmaps', (function(canvas){
+                return function(event){
+                    canvas.css('display', 'none');
+                }
+            })(canvas));
+
+        });
+        
     },
 
     resize: function(){
