dc7a8a5a911e96d1511f6b83c62ae826d0f8df64
doman412 <doman412@gmail.com>
2015-07-07 13:34:15 -0400

    found fix for Notifications mem leak

--- src/components/walls/wallListDirective.js
+++ src/components/walls/wallListDirective.js
@@ -1,55 +1,68 @@
 'use strict';
 
 var WallListDirective = BaseDirective.extend({
-  userModel: null,
-  notifications: null,
+    userModel: null,
+    notifications: null,
 
 
-  initialize: function($scope, WallModel, GymModel, UserModel, Notifications){
-    this.wallModel = WallModel;
-    this.gymModel = GymModel;
-    this.userModel = UserModel;
-    this.notifications = Notifications;
+    initialize: function($scope, WallModel, GymModel, UserModel, Notifications){
+        this.wallModel = WallModel;
+        this.gymModel = GymModel;
+        this.userModel = UserModel;
+        this.notifications = Notifications;
 
-    this.getWalls();
-  },
+        this.getWalls();
+    },
 
-  defineListeners: function(){
-    this.notifications.addEventListener(models.events.USER_SIGNED_IN, this.handleUserChange.bind(this));
-    this.notifications.addEventListener(models.events.USER_SIGNED_OUT, this.handleUserChange.bind(this));
-    this.notifications.addEventListener(models.events.WALLS_LOADED, this.handleWallsChange.bind(this));
-  },
+    defineListeners: function(){
+        this.boundHandleUserChange = this.handleUserChange.bind(this);
+        this.boundHandleWallsChange = this.handleWallsChange.bind(this);
 
-  defineScope: function(){
-    this.$scope.walls = [];
-  },
+        this.notifications.addEventListener(models.events.USER_SIGNED_IN, this.boundHandleUserChange);
+        this.notifications.addEventListener(models.events.USER_SIGNED_OUT, this.boundHandleUserChange);
+        this.notifications.addEventListener(models.events.WALLS_LOADED, this.boundHandleWallsChange);
+    },
 
-  getWalls: function(){
-    this.wallModel.getWallsByGym(this.gymModel.gym);
-  },
+    defineScope: function(){
+        this.$scope.walls = [];
+    },
 
-  /** EVENT HANDLERS **/
-  handleUserChange: function(){
-    this.$scope.currentUser = this.userModel.currentUser;
-  },
+    destroy:function(){
+        console.log('destroy wall list directive');
+        this.notifications.removeEventListener(models.events.USER_SIGNED_IN, this.boundHandleUserChange);
+        this.notifications.removeEventListener(models.events.USER_SIGNED_OUT, this.boundHandleUserChange);
+        this.notifications.removeEventListener(models.events.WALLS_LOADED, this.boundHandleWallsChange);
+    },
 
-  handleWallsChange: function(){
-    this.$scope.walls = this.wallModel.wallsByGym[this.gymModel.gym.id];
-    // console.log(this.$scope.walls);
+    getWalls: function(){
+        this.wallModel.getWallsByGym(this.gymModel.gym);
+    },
 
-  }
+    /** EVENT HANDLERS **/
+    handleUserChange: function(){
+        this.$scope.currentUser = this.userModel.currentUser;
+    },
+
+    handleWallsChange: function(){
+        this.$scope.walls = this.wallModel.wallsByGym[this.gymModel.gym.id];
+        console.log(this.$scope.walls);
+
+    }
 
 });
 
 angular.module('wallList',[])
 .directive('wallList', function(WallModel, GymModel, UserModel, Notifications){
-  return {
-    restrict:'E',
-    isolate:true,
-    link: function($scope){
-      new WallListDirective($scope, WallModel, GymModel, UserModel, Notifications);
-    },
-    scope:true,
-    templateUrl: "partials/walls/wallList.html"
-  };
+
+    var obj;
+
+    return {
+        restrict:'E',
+        isolate:true,
+        link: function($scope){
+            obj = new WallListDirective($scope, WallModel, GymModel, UserModel, Notifications);
+        },
+        scope:true,
+        templateUrl: "partials/walls/wallList.html"
+    };
 });
