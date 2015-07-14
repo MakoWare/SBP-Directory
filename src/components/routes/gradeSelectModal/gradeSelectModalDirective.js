f9b8a68fb78151e58a45c0d3026404cfe57ba023
yintothayang <yintothayang@gmail.com>
2015-07-13 21:50:37 -0700

    added ABP icons and selector

--- src/components/routes/gradeSelectModal/gradeSelectModalDirective.js
+++ src/components/routes/gradeSelectModal/gradeSelectModalDirective.js
@@ -9,11 +9,12 @@ var GradeSelectModalDirective = BaseDirective.extend({
     userModel: null,
     notifications: null,
 
-    initialize: function($scope, $elm, $state, UserModel, RouteModel,Notifications){
+    initialize: function($scope, $elm, $state, UserModel, RouteModel, GymModel, Notifications){
         this.$elm = $elm;
         this.$state = $state;
         this.userModel = UserModel;
         this.routeModel = RouteModel;
+        this.gymModel = GymModel;
         this.notifications = Notifications;
     },
 
@@ -23,22 +24,42 @@ var GradeSelectModalDirective = BaseDirective.extend({
     },
 
     defineScope: function(){
-        this.$scope.grades = [
-            [
-                ['gray0'],
-                ['yellow0', 'yellow1', 'yellow2'],
-                ['green2', 'green3', 'green4'],
-                ['red3', 'red4', 'red5']
-            ],
-            [
-                ['blue4', 'blue5', 'blue6'],
-                ['orange5', 'orange6', 'orange7'],
-                ['purple7', 'purple8'],
-                ['black8', 'black9', 'black10'],
-                ['black11', 'black12', 'black13']
-            ]
-        ];
-        this.order = ['gray', 'yellow', 'green', 'red', 'blue', 'orange', 'purple', 'black'];
+        if(this.gymModel.gym.get("name") == "Seattle Bouldering Project"){
+            this.$scope.grades = [
+                [
+                    ['gray0'],
+                    ['yellow0', 'yellow1', 'yellow2'],
+                    ['green2', 'green3', 'green4'],
+                    ['red3', 'red4', 'red5']
+                ],
+                [
+                    ['blue4', 'blue5', 'blue6'],
+                    ['orange5', 'orange6', 'orange7'],
+                    ['purple7', 'purple8'],
+                    ['black8', 'black9', 'black10'],
+                    ['black11', 'black12', 'black13']
+                ]
+            ];
+            this.order = ['gray', 'yellow', 'green', 'red', 'blue', 'orange', 'purple', 'black'];
+        } else {
+            this.$scope.grades = [
+                [
+                    ['yellow0'],
+                    ['red0', 'red1', 'red2'],
+                    ['green1', 'green2', 'green3'],
+                    ['purple2', 'purple3', 'purple4'],
+                    ['orange3', 'orange4', 'orange5']
+                ],
+                [
+                    ['black4', 'black5', 'black6'],
+                    ['blue5', 'blue6', 'blue7'],
+                    ['pink6', 'pink7', 'pink8'],
+                    ['white7', 'white8', 'white9'],
+                    ['white10', 'white11', 'white12']
+                ]
+            ];
+            this.order = ['yellow', 'red', 'green', 'purple', 'orange', 'black', 'blue', 'pink', 'white'];
+        }
     },
 
     destroy: function(){
@@ -66,12 +87,12 @@ var GradeSelectModalDirective = BaseDirective.extend({
 });
 
 angular.module('gradeSelectModal',[])
-    .directive('gradeSelectModal', ['$state', 'UserModel', 'RouteModel', 'Notifications', function($state, UserModel, RouteModel, Notifications){
+    .directive('gradeSelectModal', ['$state', 'UserModel', 'RouteModel', 'GymModel', 'Notifications', function($state, UserModel, RouteModel, GymModel, Notifications){
         return {
             restrict:'E',
             isolate:true,
             link: function($scope, $elm){
-                new GradeSelectModalDirective($scope, $elm, $state, UserModel, RouteModel, Notifications);
+                new GradeSelectModalDirective($scope, $elm, $state, UserModel, RouteModel, GymModel, Notifications);
             },
             scope: true,
             templateUrl: "partials/routes/gradeSelectModal.html"
