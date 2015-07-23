8e738806b95cfac479c4458e290fcde6e66988c5
yintothayang <yintothayang@gmail.com>
2015-07-23 10:01:50 -0700

    wallsController for derek

---
+++ src/components/walls/wallsController.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var WallsController = BaseController.extend({
+    notifications:null,
+
+    initialize: function($scope, $state, Notifications, GymModel, WallModel){
+        this.$state = $state;
+        this.wallModel = WallModel;
+        this.gymModel = GymModel;
+        this.notifications = Notifications;
+    },
+
+    defineListeners:function(){
+
+    },
+
+    defineScope:function(){
+        this.$scope.gym = this.gymModel.gym;
+        this.$scope.createWall = this.createWall.bind(this);
+        this.notifications.notify(models.events.BRAND_CHANGE, this.$scope.gym.get("name"));
+    },
+
+    destroy:function(){
+
+    },
+
+    createWall: function(){
+        console.log("create wall");
+        this.wallModel.createWall(this.$scope.gym).then(function(wall){
+            console.log(wall);
+            this.$state.go('wall', {wallId: wall.id });
+        }.bind(this));
+    }
+
+});
+
+
+WallsController.$inject = ['$scope', '$state', 'Notifications', 'GymModel', 'WallModel'];
